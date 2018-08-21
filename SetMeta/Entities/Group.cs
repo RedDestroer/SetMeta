24fc214610fdd352689f238b54517c9c0923ca4c
Sergei Iakovlev <reddestroer@mail.ru>
2018-08-21 18:49:09 +0300

    T-017 added groups

--- SetMeta/Entities/Group.cs
+++ SetMeta/Entities/Group.cs
@@ -1,9 +1,17 @@
-﻿namespace SetMeta.Entities
+﻿using System.Collections.Generic;
+
+namespace SetMeta.Entities
 {
     public class Group
     {
+        public Group()
+        {
+            Options = new Dictionary<string, Option>();
+        }
+
         public string Name { get; set; }
         public string DisplayName { get; set; }
         public string Description { get; set; }
+        public IDictionary<string, Option> Options { get; }
     }
 }
--- SetMeta/Entities/OptionSet.cs
+++ SetMeta/Entities/OptionSet.cs
@@ -6,10 +6,12 @@ namespace SetMeta.Entities
     {
         public OptionSet()
         {
+            Groups = new Dictionary<string, Group>();
             Options = new Dictionary<string, Option>();
         }
 
         public string Version { get; set; }
+        public IDictionary<string, Group> Groups { get; }
         public IDictionary<string, Option> Options { get; }
     }
 }
