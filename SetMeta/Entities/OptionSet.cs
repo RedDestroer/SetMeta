43bbdd3e638544106b9a24589e9a54ba28b1c55e
Sergei Iakovlev <reddestroer@mail.ru>
2018-08-22 14:58:05 +0300

    T-017 groups added

--- SetMeta/Entities/OptionSet.cs
+++ SetMeta/Entities/OptionSet.cs
@@ -6,10 +6,12 @@ namespace SetMeta.Entities
     {
         public OptionSet()
         {
-Options = new List<Option>();
+            Options = new List<Option>();
+            Groups = new List<Group>();
         }
 
         public string Version { get; set; }
- public IList<Option> Options { get; }
+        public IList<Option> Options { get; }
+        public IList<Group> Groups { get; }
     }
 }
