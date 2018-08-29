984f3d1f3eaa2a476cec6e5cfdaf423da4f3edbd
floranRV <floranRV@gmail.com>
2018-08-29 21:38:40 +0300

    T-029 Added Suggestions and tests for them

---
+++ SetMeta/Entities/Suggestions/MinLengthSuggestion.cs
@@ -0,0 +1,16 @@
+﻿namespace SetMeta.Entities.Suggestions
+{
+    public class MinLengthSuggestion
+        :Suggestion
+    {
+        /// <summary>
+        /// Тип предложения
+        /// </summary>
+        public override SuggestionType SuggestionType => SuggestionType.MinLength;
+
+        /// <summary>
+        /// Желаемая минимальная длина поля
+        /// </summary>
+        public ushort Value { get; set; }
+    }
+}
---
+++ SetMeta/Entities/Suggestions/NotifyOnChangeSuggestion.cs
@@ -0,0 +1,11 @@
+﻿namespace SetMeta.Entities.Suggestions
+{
+    public class NotifyOnChangeSuggestion
+        :Suggestion
+    {
+        /// <summary>
+        /// Тип предложения
+        /// </summary>
+        public override SuggestionType SuggestionType => SuggestionType.NotifyOnChange;
+    }
+}
