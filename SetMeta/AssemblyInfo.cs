9fe92261a9818a6ed574f9a3ec8e732a4dd686c7
Sergei Iakovlev <RedDestroer@mail.ru>
2018-11-18 19:47:49 +0300

    T 052 (#52)
    * T-012 Added more OptionValueTypes and new test for them
    
    * T-012 Corrected the mistakes
    
    * T-028 Added Contant parser and tests for it
    
    * T-028 Corrected mistake with id
    
    * T-052 Added versions to AssamblyInfo

--- SetMeta/AssemblyInfo.cs
+++ SetMeta/AssemblyInfo.cs
@@ -1,3 +1,36 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("SetMeta.Tests")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SetMeta")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Run-Retail")]
+[assembly: AssemblyProduct("SetMeta")]
+[assembly: AssemblyCopyright("Copyright © Run-Retail 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
